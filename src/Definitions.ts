d8b9b3870a6bb2b2e4931caed2ae10cc0cbe1f12
xulihang <xulihanghai@163.com>
2023-02-22 16:05:11 +0800

    basic template with microbundle

---
+++ src/Definitions.ts
@@ -0,0 +1,15 @@
+export interface Point {
+  x:number;
+  y:number;
+}
+
+export interface Polygon {
+  points:Point[]
+}
+
+export interface Rectangle {
+  x:number;
+  y:number;
+  width:number;
+  height:number;
+}
---
+++ src/ShapeSorter.ts
@@ -0,0 +1,10 @@
+import { Polygon, Rectangle } from "./Definitions";
+
+export default class ShapeSorter {
+  sortPolygons(polygons:Polygon[]): Polygon[] {
+    return [];
+  }
+  sortRectangles(rectangles:Rectangle[]): Rectangle[] {
+    return [];
+  }
+}
