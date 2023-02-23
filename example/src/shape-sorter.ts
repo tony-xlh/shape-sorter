4383d04bd0bb840119f0e3acdb3b786b5dd04b4d
xulihang <xulihanghai@163.com>
2023-02-23 15:29:08 +0800

    sort rectangles base on lines

--- example/src/shape-sorter.ts
+++ example/src/shape-sorter.ts
@@ -14,8 +14,66 @@ export default class ShapeSorter {
   sortRectangles(rectangles:Rectangle[]): Mapping[] {
     rectangles.sort((a, b) => (a.x**2 + a.y**2) - (b.x**2 + b.y**2))
     console.log(rectangles);
-    return this.getMappingsFromRectangles(rectangles);
+    let lines = this.getLines(rectangles);
+    console.log(lines);
+    return this.getMappingsFromRectangles(this.getRectangesFromLines(lines));
   }
+
+  getRectangesFromLines(lines:Rectangle[][]):Rectangle[]{
+    let rectangles:Rectangle[] = [];
+    for (let index = 0; index < lines.length; index++) {
+      const line = lines[index];
+      rectangles = rectangles.concat(line);
+    }
+    return rectangles;
+  }
+
+  getLines(rectangles:Rectangle[]):Rectangle[][] {
+    let lines = [];
+    while (rectangles.length > 0) {
+      let rect = rectangles[0];
+      let clustered = this.clusterRectanglesToOneLine(rectangles,rect);
+      if (clustered) {
+        lines.push(clustered);
+      } else {
+        lines.push([rect]); //single rectangle as a line
+      }
+      rectangles.shift(); //delete the base rect
+    }
+    lines.sort((a, b) => (a[0].y - b[0].y));
+    return lines;
+  }
+
+  clusterRectanglesToOneLine(rectangles:Rectangle[],baseRect:Rectangle):Rectangle[]|undefined{
+    let line = [];
+    for (let index = rectangles.length - 1; index >= 1; index--) { // start from the second element
+      const rect = rectangles[index];
+      if (this.rectanglesInOneLine(baseRect,rect)) {
+        line.push(rect);
+        rectangles.splice(index, 1); //delete the rectangle
+      }
+    }
+    if (line.length>0) {
+      line.push(baseRect);
+      line.sort((a, b) => (a.x - b.x));
+      return line;
+    }else{
+      return undefined;
+    }
+  }
+
+  rectanglesInOneLine(r1:Rectangle, r2:Rectangle) {
+    let y = Math.max(r1.y, r2.y);
+    let maxY1 = r1.y + r1.height;
+    let maxY2 = r2.y + r2.height;
+    let intersectH = Math.min(maxY1, maxY2) - y;
+    if (intersectH>0) {
+      return true;
+    }else{
+      return false;
+    }
+  }
+    
 
   getMappingsFromRectangles(rectangles:Rectangle[]): Mapping[] {
     let mappings:Mapping[] = [];
